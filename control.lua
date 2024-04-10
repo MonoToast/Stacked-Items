fe20d5c150ea5b023033f01146802639da022a52
Jonathan Riedemann <monotoastjlr@gmail.com>
2024-04-10 17:28:31 -0500

    Added a .gitignore file to ignore the .vscode folder.  Internal changes, no functionality changes. Bumped version from 0.1.13 to 0.1.14.  Reduced redundance and code complexity in control.lua file.

--- control.lua
+++ control.lua
@@ -1,34 +1,16 @@
-local StackedSuffix = "-stacked"
+local function updateStackedItems(playerIndex)
+    local playerInventory = game.players[playerIndex].get_main_inventory()
 
-local function IsStacked(name)
-    return #StackedSuffix < #name and name:sub(-#StackedSuffix) == StackedSuffix
-end
+    if not playerInventory then return end
 
-local function BatchConvertStackedItems(player, items)
-    for _, item in ipairs(items) do
-        player.get_main_inventory().remove(item)
-        player.get_main_inventory().insert({name = item.name:sub(0, #item.name - #StackedSuffix), count = item.count * 4})
-    end
-end
-
-local function on_player_main_inventory_changed(event)
-
-    local player = game.players[event.player_index]
-    local inv = player.get_main_inventory().get_contents()
-
-    local itemsToConvert = {}
-
-    for name, count in pairs(inv) do
-        if IsStacked(name) then
-            table.insert(itemsToConvert, {name = name, count = count})
+    for i = 1, #playerInventory do
+        local item = playerInventory[i]
+        if item and item.valid_for_read and string.match(item.name, "-stacked$") then
+            local baseItemName = item.name:sub(1, -#"-stacked" - 1)
+            item.set_stack({name = baseItemName, count = item.count * 4})
         end
     end
-
-    BatchConvertStackedItems(player, itemsToConvert)
-
 end
-
-script.on_event(defines.events.on_player_main_inventory_changed, on_player_main_inventory_changed)
 
 local vectorTable = {
     [0] = {x = 0, y = -1},
@@ -37,50 +19,45 @@ local vectorTable = {
     [6] = {x = -1, y = 0}
 }
 
-local function CreateLoaderTable(loaderEntity)
-    local direction = (loaderEntity.direction + (loaderEntity.loader_type == "output" and 4 or 0)) % 8
-    return {
-        direction = direction,
-        normal = vectorTable[direction],
-        connectionPoint = {x = loaderEntity.position.x + vectorTable[direction].x, y = loaderEntity.position.y + vectorTable[direction].y},
-        reference = loaderEntity
+local function handleStackerEntity(placedLoader, isCreate)
+    if not placedLoader or not placedLoader.valid then return end
+
+    local placedDirection = (placedLoader.direction + (placedLoader.loader_type == "output" and 4 or 0)) % 8
+    local connectionLoader = placedLoader.surface.find_entity(placedLoader.name, {x = placedLoader.position.x + vectorTable[placedDirection].x, y = placedLoader.position.y + vectorTable[placedDirection].y})
+
+    if not connectionLoader then return end
+    local connectionDirection = (connectionLoader.direction + (connectionLoader.loader_type == "output" and 4 or 0)) % 8
+
+    if (((placedDirection + 4) % 8) - connectionDirection) ~= 0 then return end
+
+    local middlePosition = {
+        x = (placedLoader.position.x + vectorTable[placedDirection].x + connectionLoader.position.x + vectorTable[connectionDirection].x) / 2,
+        y = (placedLoader.position.y + vectorTable[placedDirection].y + connectionLoader.position.y + vectorTable[connectionDirection].y) / 2
     }
-end
 
-local function ConnectLoader(entity)
-
-    if not entity then return end
-    local loader = CreateLoaderTable(entity)
-    local findEntityResult = entity.surface.find_entity(entity.name, loader.connectionPoint)
-
-    if not findEntityResult then return end
-    local loaderToConnect = CreateLoaderTable(findEntityResult)
-
-    if not (loader.normal.x + loaderToConnect.normal.x == 0 and loader.normal.y + loaderToConnect.normal.y == 0) then return end
-
-    local pos = (loader.direction == 0 or loader.direction == 6) and loaderToConnect.reference.position or loader.reference.position
-    entity.surface.create_entity{name = "stacker-entity", position = pos, direction = loader.direction, force = entity.force}
-end
-
-local function DisconnectLoaders(entity)
-    
-    if not entity then return end
-    local loader = CreateLoaderTable(entity)
-
-    local findEntityResult = entity.surface.find_entity("stacker-entity", loader.connectionPoint) or entity.surface.find_entity("stacker-entity", loader.reference.position)
-
-    if not findEntityResult then return end
-    findEntityResult.destroy()
+    if isCreate then
+        placedLoader.surface.create_entity{
+            name = "stacker-entity",
+            position = middlePosition,
+            direction = placedDirection,
+            force = placedLoader.force
+        }
+    else
+        local stackerEntity = placedLoader.surface.find_entity("stacker-entity", middlePosition)
+        if stackerEntity then stackerEntity() end
+    end
 end
 
 local filter = {{filter = "type", type = "loader-1x1"}}
 
-script.on_event(defines.events.on_built_entity,         function (event) ConnectLoader(event.created_entity) end, filter)
-script.on_event(defines.events.on_robot_built_entity,   function (event) ConnectLoader(event.created_entity) end, filter)
-script.on_event(defines.events.script_raised_built,     function (event) ConnectLoader(event.entity)         end, filter)
-script.on_event(defines.events.script_raised_revive,    function (event) ConnectLoader(event.entity)         end, filter)
+script.on_event(defines.events.on_player_main_inventory_changed, updateStackedItems)
 
-script.on_event(defines.events.script_raised_destroy,   function (event) DisconnectLoaders(event.entity) end, filter)
-script.on_event(defines.events.on_player_mined_entity,  function (event) DisconnectLoaders(event.entity) end, filter)
-script.on_event(defines.events.on_robot_mined_entity,   function (event) DisconnectLoaders(event.entity) end, filter)
-script.on_event(defines.events.on_entity_died,          function (event) DisconnectLoaders(event.entity) end, filter)
+script.on_event(defines.events.on_built_entity, function(event) handleStackerEntity(event.created_entity, true) end, filter)
+script.on_event(defines.events.on_robot_built_entity, function(event) handleStackerEntity(event.created_entity, true) end, filter)
+script.on_event(defines.events.script_raised_built, function(event) handleStackerEntity(event.entity, true) end, filter)
+script.on_event(defines.events.script_raised_revive, function(event) handleStackerEntity(event.entity, true) end, filter)
+
+script.on_event(defines.events.script_raised_destroy, function(event) handleStackerEntity(event.entity, false) end, filter)
+script.on_event(defines.events.on_player_mined_entity, function(event) handleStackerEntity(event.entity, false) end, filter)
+script.on_event(defines.events.on_robot_mined_entity, function(event) handleStackerEntity(event.entity, false) end, filter)
+script.on_event(defines.events.on_entity_died, function(event) handleStackerEntity(event.entity, false) end, filter)
